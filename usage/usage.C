a73f53cdccf9b2b24f683df8ce366bc62b4c8f35
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2012-11-27 14:43:29 +0100

    tool for attributing disk usage to groups and users

---
+++ usage/usage.C
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <sstream>
+#include <string>
+#include <fstream>
+
+//#define DEBUG 
+
+const int gid_etmc = 3585;
+const int gid_nic = 1414;
+
+double etmc_size = 0.0;
+double nic_size = 0.0;
+double other_size = 0.0;
+
+int main(int argc, char* argv[]) {
+  if( argc < 2 ) {
+    printf("USAGE:\n");
+    printf("\t ./usage findlist\n");
+    printf("\t where findlist is the output of find $DIRECTORY\n");
+    exit(0);
+  }
+ 
+  std::string line;
+  std::ifstream findlist(argv[1]);
+
+  struct stat fstat;
+  
+  while(findlist >> line) {
+    stat(line.c_str(),&fstat);
+    if( S_ISREG(fstat.st_mode) ) {
+        if( fstat.st_gid == gid_etmc ) {
+          etmc_size += 1.0e-12*(double)fstat.st_size;
+        } else if ( fstat.st_gid == gid_nic ) {
+          nic_size += 1.0e-12*(double)fstat.st_size;
+        } else {
+          other_size += 1.0e-12*(double)fstat.st_size;
+        }
+#ifdef DEBUG
+      printf("Filename: %s, Size: %lf, Gid: %d, Attbs: %ud \n",line.c_str(),(double)fstat.st_size,fstat.st_gid,fstat.st_mode);
+#endif
+    }
+  }
+
+  printf("%lf %lf %lf\n",etmc_size,nic_size,other_size);
+
+  return(0);
+}
---
+++ usage/usage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ -z ${1} || -z ${2} ]]; then
+  echo "USAGE:"
+  echo "  sh usage.sh DIRECTORY REFIND_FLAG [0,1]"
+  echo "  REFIND_FLAG indicates (1) whether find should be run on subdirectories to (re)create findlist_*.txt's"
+  exit 0
+fi
+
+touch usage.txt
+
+PWD=`pwd`
+
+for i in `ls -1 ${PWD}`; do
+  if [[ -d ${i} && ! ${i} = *..* && ! ${i} = *.* ]]; then
+    if [[ ${2} -eq 1 ]]; then
+      echo "Calling find for ${i}"
+      find `pwd`/${i} -print > findlist_${i}.txt
+    fi
+    echo "Calling usage for ${i}"
+    usage_out=`$HOME/code/utils/usage/usage findlist_${i}.txt`
+    echo ${i} ${usage_out} >> usage.txt
+  fi
+done
+
