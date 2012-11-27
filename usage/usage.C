99f48922d7b71abf3730e5f769217deef587501e
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2012-11-27 15:30:01 +0100

    provide file count in addition to size

--- usage/usage.C
+++ usage/usage.C
@@ -16,6 +16,10 @@ double etmc_size = 0.0;
 double nic_size = 0.0;
 double other_size = 0.0;
 
+unsigned long int etmc_fcount = 0;
+unsigned long int nic_fcount = 0;
+unsigned long int other_fcount = 0;
+
 int main(int argc, char* argv[]) {
   if( argc < 2 ) {
     printf("USAGE:\n");
@@ -34,10 +38,13 @@ int main(int argc, char* argv[]) {
     if( S_ISREG(fstat.st_mode) ) {
         if( fstat.st_gid == gid_etmc ) {
           etmc_size += 1.0e-12*(double)fstat.st_size;
+          ++etmc_fcount;
         } else if ( fstat.st_gid == gid_nic ) {
           nic_size += 1.0e-12*(double)fstat.st_size;
+          ++nic_fcount;
         } else {
           other_size += 1.0e-12*(double)fstat.st_size;
+          ++other_fcount;
         }
 #ifdef DEBUG
       printf("Filename: %s, Size: %lf, Gid: %d, Attbs: %ud \n",line.c_str(),(double)fstat.st_size,fstat.st_gid,fstat.st_mode);
@@ -45,7 +52,7 @@ int main(int argc, char* argv[]) {
     }
   }
 
-  printf("%lf %lf %lf\n",etmc_size,nic_size,other_size);
+  printf("%lf %lf %lf %ld %ld %ld\n",etmc_size,nic_size,other_size,etmc_fcount,nic_fcount,other_fcount);
 
   return(0);
 }
