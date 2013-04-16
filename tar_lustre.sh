9dc9cd246bf1d1a0d0566e19e2ff97562084da3a
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2013-04-16 16:58:41 +0200

    add utility for slightly more efficient archival of directories on lustre filesystems by appending

---
+++ tar_lustre.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if test ${1} = ""; then
+  echo "usage:"
+  echo "tar_lustre.sh <dirname>"
+  echo "   where dirname is the name of the directory to tar"
+  exit 1
+fi
+
+find ${1} -type f -fprint ${1}_findlist.txt
+tar -cf ${1}.tar `head -n 1 ${1}_findlist.txt`
+tar --delete -f ${1}.tar `head -n 1 ${1}_findlist.txt`
+cat ${1}_findlist.txt | xargs tar -r -f ${1}.tar
+
