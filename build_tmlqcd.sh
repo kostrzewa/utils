8c2cb32e5d809563e04f6c351ad83a05b6052c89
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2014-11-21 16:10:32 +0100

    build_tmlqcd.sh: full support for MKL

--- build_tmlqcd.sh
+++ build_tmlqcd.sh
@@ -23,7 +23,7 @@ BENCHDIR="${EDIR}/benchmark_${ADDON}"
 LEMONDIR="--with-lemondir=/afs/ifh.de/user/k/kostrzew/local64_pax/lemon"
 
 
-commonflags="--enable-gaugecopy --disable-p4 --enable-alignment=32 --without-gprof --with-lapack=-L/usr/lib64/atlas --with-limedir=/afs/ifh.de/user/k/kostrzew/local64_pax/lime_icc"
+commonflags="--enable-gaugecopy --disable-p4 --enable-alignment=32 --without-gprof --with-limedir=/afs/ifh.de/user/k/kostrzew/local64_pax/lime_icc"
 mpiflags=""
 openmpflags=""
 hsflag=""
@@ -32,6 +32,7 @@ cflags=""
 ldflags=""
 cc=""
 ccpath=""
+f77=""
 
 if [[ ! -d ${HMCDIR} ]]; then
   mkdir -p ${HMCDIR}
@@ -52,6 +53,7 @@ fi
 if [[ $COMP == "icc" ]]; then
   echo "using the Intel compiler 2013"
   sseflags="--disable-sse2 --disable-sse3"
+  f77=ifort
   eval `modulecmd sh add intel.2013`
   if [[ $MPI == "mvapich2" ]]; then
     echo "linking against mvapich2"
@@ -67,10 +69,11 @@ if [[ $COMP == "icc" ]]; then
     echo "no MPI library specified in MPI variable, exiting (openmpi/mvapich2)"
     exit 1
   fi
-  source /usr/local/bin/intel-setup-2013.sh intel64
+  . /opt/intel/2013/bin/iccvars.sh intel64
 elif [[ $COMP = "gcc" ]]; then
   echo "using the GCC compiler"
   intel=0
+  f77=g77
   sseflags="--enable-sse2 --enable-sse3"
   if [[ $MPI == "mvapich2" ]]; then
     echo "linking against mvapich2"
@@ -95,7 +98,7 @@ for i in ${versions}; do
   cc=""
   ldflags=""
   if [[ $COMP = "icc" ]]; then
-    cflags="-std=c99 -march=corei7 -O3"
+    cflags="-std=c99 -march=corei7 -O3 -mkl"
   else
     cflags="-std=c99 -O3 -mtune=core2"
   fi
@@ -139,12 +142,24 @@ for i in ${versions}; do
     if [[ $COMP = "icc" ]]; then
       cflags="${cflags}"
       ldflags="${ldflags}"
+      # MKL static threaded
+      #commonflags="${commonflags} --with-lapack='-Wl, --start-group /opt/intel/2013/mkl/lib/intel64/libmkl_blas95_lp64.a /opt/intel/2013/mkl/lib/intel64/libmkl_intel_lp64.a /opt/intel/2013/mkl/lib/intel64/libmkl_core.a /opt/intel/2013/mkl/lib/intel64/libmkl_intel_thread.a -Wl, --end-group'"
+      # MKL runtime 
+      #commonflags="${commonflags} --with-lapack='-L/opt/intel/2013/mkl/lib/intel64 -lmkl_rt'"
+      # MKL dynamic threaded
+      commonflags="${commonflags} --with-lapack='-L/opt/intel/2013/composer_xe_2013_sp1.3.174/mkl/lib/intel64 -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread'"
     else
       cflags="${cflags} -fopenmp"
       ldflags="${ldflags} -fopenmp"
     fi
   else
     openmpflags="${openmpflags} --disable-omp"
+    # MKL static, sequential
+    #commonflags="${commonflags} --with-lapack='-Wl, --begin-group /opt/intel/2013/mkl/lib/intel64/libmkl_blas95_lp64.a /opt/intel/2013/mkl/lib/intel64/libmkl_intel_lp64.a /opt/intel/2013/mkl/lib/intel64/libmkl_core.a /opt/intel/2013/mkl/lib/intel64/libmkl_sequential.a -Wl, --end-group '"
+    # MKL run-time selection
+    #commonflags="${commonflags} --with-lapack='-L/opt/intel/2013/mkl/lib/intel64 -lmkl_rt'"
+    # MKL dynamic, sequential
+    commonflags="${commonflags} --with-lapack='-L/opt/intel/2013/composer_xe_2013_sp1.3.174/mkl/lib/intel64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential'"
   fi
 
   case ${i} in
@@ -162,18 +177,27 @@ for i in ${versions}; do
     ;;
   esac
   
-  echo "executing configure for $i" &&
-  echo "CFLAGS=${cflags} LDFLAGS=${ldflags} CC=${cc} ${SDIR}/configure ${commonflags} ${mpiflags} ${openmpflags} ${hsflag} ${sseflags}"
-  CFLAGS="${cflags}" LDFLAGS="${ldflags}" CC="${cc}" ${SDIR}/configure ${commonflags} ${mpiflags} ${openmpflags} ${hsflag} ${sseflags} &&
+  echo "executing configure for $i"
+  configurecommand="CFLAGS=\"${cflags}\" LDFLAGS=\"${ldflags}\" CC=${cc} F77=${f77} ${SDIR}/configure ${commonflags} ${mpiflags} ${openmpflags} ${hsflag} ${sseflags}"
+  echo ${configurecommand}
+  eval ${configurecommand}
 
-  echo "beginning compilation for $i" &&
-  make -j9 &&
+  CONF_ESTAT=$?
+
+  echo "beginning compilation for $i"
+  make -j9
+
+  COMP_ESTAT=$?
+
   cp hmc_tm ${HMCDIR}/${i} &&
   cp benchmark ${BENCHDIR}/${i} &&
   cp invert ${INVDIR}/${i}
 
-  if [ $? -ne 0 ]; then
-    echo "non-zero exit status somewhere, exiting!"
-    exit $?
+  CP_ESTAT=$?
+
+  if [ $CP_ESTAT -ne 0 -o $COMP_ESTAT -ne 0 -o $CONF_ESTAT -ne 0  ]; then
+    echo "non-zero exit status somewhere while compiling ${i}, exiting!"
+    echo -e " CONF_ESTAT = $CONF_ESTAT \n COMP_ESTAT = $COMP_ESTAT \n CP_ESTAT = $CP_ESTAT"
+    exit 129
   fi
 done
